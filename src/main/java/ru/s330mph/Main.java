e76672ad5844f19cc6c4aa53f12cdd33390058da
s330mph <s330mph@gmail.com>
2025-06-15 09:50:57 +0300

    hello message change to 3 dots

--- src/main/java/ru/s330mph/Main.java
+++ src/main/java/ru/s330mph/Main.java
@@ -2,7 +2,7 @@ package ru.s330mph;
 
 public class Main {
 
-    private static String msg = "Hello qa.guru!";
+    private static String msg = "Hello qa...!";
 
     public static void main(String... args) {
         System.out.println(msg);
