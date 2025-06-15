d2cfe15d95973d0f32d036c3a0807f350e39c2b4
s330mph <s330mph@gmail.com>
2025-06-15 08:24:32 +0200

    add project

---
+++ src/main/java/ru/s330mph/Main.java
@@ -0,0 +1,10 @@
+package ru.s330mph;
+
+public class Main {
+
+    private static String msg = "Hello qa.guru!";
+
+    public static void main(String... args) {
+        System.out.println(msg);
+    }
+}
