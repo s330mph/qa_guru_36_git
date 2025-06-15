12006f7990f0c172c13192be6b35a8e83bea55de
s330mph <s330mph@gmail.com>
2025-06-15 10:17:49 +0300

    add getmessage method

--- src/main/java/ru/s330mph/Main.java
+++ src/main/java/ru/s330mph/Main.java
@@ -1,10 +1,17 @@
 package ru.s330mph;
 
+import jdk.dynalink.beans.StaticClass;
+
+import java.util.Stack;
+
 public class Main {
 
     private static String msg = "Hello, stage2 here!";
 
     public static void main(String... args) {
         System.out.println(msg);
+    }
+    private static String getMsg() {
+        return "qqqq";
     }
 }
