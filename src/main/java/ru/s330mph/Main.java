65d834e8e82ba8a98a209fd757e1b698e1ea1115
s330mph <s330mph@gmail.com>
2025-06-15 09:17:05 +0200

    Update Main.java

--- src/main/java/ru/s330mph/Main.java
+++ src/main/java/ru/s330mph/Main.java
@@ -2,7 +2,7 @@ package ru.s330mph;
 
 public class Main {
 
-    private static String msg = "Hello, stage2 here!";
+    private static String msg = "Hello, stage2 here!!!!!!";
 
     public static void main(String... args) {
         System.out.println(msg);
